971cb5c7841d09cf685d7c161599c3b28fb8cd7b
Ayrton_Calderon <ayrtoncalderon@gmail.com>
2025-08-23 20:51:25 -0300

    Se han agregado 10 ejercicios al repositorio

--- Ejercicio_complementario/Ejercicio_secuenciales_Calderon_Ayrton.py
+++ Ejercicio_complementario/Ejercicio_secuenciales_Calderon_Ayrton.py
@@ -55,3 +55,32 @@ print(f"La division entre numero1 y numero2 es de: {division}")
 #Ejercicio 5: Crea una variable llamada "nombre" y asígnale tu nombre como valor.
 nombre= "Ayrton"
 print(f"El nombre ingresado es: {nombre}")
+
+#----------------------------------------------------------------------------
+#Ejercicio 6: Crea una variable llamada "precio" y asígnale un valor decimal que represente el precio de un artículo ficticio.
+precio=float(input("Ingrese el precio de un producto: "))
+
+#----------------------------------------------------------------------------
+#Ejercicio 7: Ahora, sin borrar la variable anterior, crea una variable llamada "descuento" y asígnale
+#un valor decimal que represente el descuento aplicado al artículo. Por ejemplo, si le
+#quieres aplicar un 25% de descuento, dale un valor de 0,25. El valor 1 equivaldría al 100% y el valor 0 al 0%.
+descuento=float(input("Ingrese el valor de descuento: "))
+#----------------------------------------------------------------------------
+
+#Ejercicio 8: Ahora, intenta calcular el precio final aplicando el descuento al precio original y
+#almacena el resultado en una variable llamada "precio_final". Para ello vas a tener que aplicar la lógica de matemáticas.
+precio_final = precio * (1 - descuento)
+#descuento: Si el descuento es 0.25 → 1 - 0.25 = 0.75
+#Eso significa que vas a pagar el 75% del precio original.
+print(f"El precio final con descuento es: {precio_final}")
+
+#----------------------------------------------------------------------------
+#Ejercicio 9:Crea una variable llamada "cadena" y asignale un texto, una frase, lo que quieras de tu elección. Qué sea un string.
+
+cadena="Soy ayrton calderon, tengo 28 años y vivo en Mendoza, Argentina"
+print(f"Se ha escrito: {cadena}")
+#----------------------------------------------------------------------------
+#Ejercicio 10: Sin borrar la variable "cadena", crea una nueva variable llamada "longitud". En ella, vas
+# a almacenar la longitud en caracteres de la cadena utilizando una de las funciones de Python.
+longitud=len(cadena)
+print(f"La longitud de la cadena es: {longitud}")
